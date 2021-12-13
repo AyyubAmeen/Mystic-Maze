ce05b86eb4f66318d1da93a22748072c0a394f46
Ogladian <ayuubumar1234@gmail.com>
2021-12-13 14:01:43 +0000

    Minor Modifications + Got rid of "Old Main.py"

--- Main.py
+++ Main.py
@@ -16,6 +16,7 @@ class map:
         self.floorNum = 0
         self.floor2D = []
         self.floor1D = []  
+        self.roomCont = []
         
     def generate(self):
         self.floorList()
@@ -28,67 +29,66 @@ class map:
 class player(stats):
     def __init__(self, hp, atk, defe, spd):
         stats.__init__(self, hp, atk, defe, spd)
-        self.velX = 0
-        self.velY = 0
         self.vel = 5
         self.width = 64
         self.height = 64
-        self.noSprites = 36
-        self.spriteScale = 2 
+        self.spriteScale = 2
+        self.faceLeft, self.faceRight, self.faceUp, self.faceDown = False, False, False, False
+        self.left, self.right, self.up, self.down = False, False, False, False
         self.currentFrame = 0
         self.lastUpdated = 0
         self.state = "idle"
         self.rightIdleList = [pygame.image.load(os.path.join('Assets', 'Player Frames', 'Wizard05.png')),
                               pygame.image.load(os.path.join('Assets', 'Player Frames', 'Wizard06.png'))]
+
         self.downIdleList = [pygame.image.load(os.path.join('Assets', 'Player Frames', 'Wizard00.png')),
                              pygame.image.load(os.path.join('Assets', 'Player Frames', 'Wizard01.png'))]
+
         self.upIdleList = [pygame.image.load(os.path.join('Assets', 'Player Frames', 'Wizard10.png')),
                            pygame.image.load(os.path.join('Assets', 'Player Frames', 'Wizard11.png'))]
+
         self.rightList = [pygame.image.load(os.path.join('Assets', 'Player Frames', 'Wizard07.png')),
                           pygame.image.load(os.path.join('Assets', 'Player Frames', 'Wizard08.png')),
-                          pygame.image.load(os.path.join('Assets', 'Player Frames', 'Wizard09.png'))]
+                          pygame.image.load(os.path.join('Assets', 'Player Frames', 'Wizard09.png')),
+                          pygame.image.load(os.path.join('Assets', 'Player Frames', 'Wizard08.png'))]
+
         self.downList = [pygame.image.load(os.path.join('Assets', 'Player Frames', 'Wizard02.png')),
                          pygame.image.load(os.path.join('Assets', 'Player Frames', 'Wizard03.png')),
-                         pygame.image.load(os.path.join('Assets', 'Player Frames', 'Wizard04.png'))]
+                         pygame.image.load(os.path.join('Assets', 'Player Frames', 'Wizard04.png')),
+                         pygame.image.load(os.path.join('Assets', 'Player Frames', 'Wizard03.png'))]
+
         self.upList = [pygame.image.load(os.path.join('Assets', 'Player Frames', 'Wizard12.png')),
                        pygame.image.load(os.path.join('Assets', 'Player Frames', 'Wizard13.png')),
-                       pygame.image.load(os.path.join('Assets', 'Player Frames', 'Wizard14.png'))]
+                       pygame.image.load(os.path.join('Assets', 'Player Frames', 'Wizard14.png')),
+                       pygame.image.load(os.path.join('Assets', 'Player Frames', 'Wizard13.png'))]
+
         self.currentSprite = self.downIdleList[0] 
-        self.faceLeft, self.faceRight, self.faceUp, self.faceDown = False, False, False, False
-        self.left, self.right, self.up, self.down = False, False, False, False
         
     def update(self, playerRect, keysPressed):
-        self.setState()
         self.movement(playerRect, keysPressed)
         self.animate()    
+
+    def draw(self, playerRect, window):    
+        window.blit(pygame.transform.scale(self.currentSprite, (self.width,self.height)), (playerRect.x, playerRect.y))
         
     def movement(self, playerRect, keysPressed):
         self.velX = 0
         self.velY = 0
+        self.state = "idle"
         if keysPressed[pygame.K_a]:
             self.velX += -self.vel * self.spd
+            self.state = "left"           
         if keysPressed[pygame.K_d]:
             self.velX += self.vel * self.spd
+            self.state = "right"  
         if keysPressed[pygame.K_s]:
             self.velY += self.vel * self.spd
+            self.state = "down"
         if keysPressed[pygame.K_w]:
             self.velY += -self.vel * self.spd
+            self.state = "up"
         playerRect.x += self.velX
         playerRect.y += self.velY
-        
-    def draw(self, playerRect, window):    
-        window.blit(pygame.transform.scale(self.currentSprite, (self.width,self.height)), (playerRect.x, playerRect.y))
-        
-    def setState(self):
-        self.state = "idle"
-        if self.velX < 0:
-            self.state = "left"           
-        elif self.velX > 0:
-            self.state = "right"  
-        if self.velY > 0:
-            self.state = "down"  
-        if self.velY < 0:
-            self.state = "up"    
 
     def animate(self): 
         currentTime = pygame.time.get_ticks()      
@@ -118,13 +118,12 @@ class player(stats):
                     self.currentSprite = self.upList[self.currentFrame]
 
 class playerProjectile:
-    def __init__(self, projRange, projLife, projDmg, projShots):
-        self.projRange = projRange
+    def __init__(self, projLife, baseDmg, noShots, spd, size):
         self.projLife = projLife
-        self.projDmg = projDmg
-        self.projShots = projShots 
-        self.projSpd = 5
-        self.projSize = 7
+        self.baseDmg = baseDmg
+        self.noShots = noShots 
+        self.spd = spd
+        self.size = size
         self.projList = []
         
     def update(self):
@@ -147,7 +146,9 @@ class playerProjectile:
         for posX, posY, projVelX, projVelY in self.projList:
             posX = int(posX)
             posY = int(posY)
-            pygame.draw.circle(window, colour, (posX, posY), self.projSize)
+            pygame.draw.circle(window, colour["yellow"], (posX, posY), (self.projSize * (5/3)))
+            pygame.draw.circle(window, colour["orange"], (posX, posY), (self.projSize * (4/3)))
+            pygame.draw.circle(window, colour["red"], (posX, posY), self.projSize)
                        
 class enemy(stats):
     def __init__(self, hp, atk, defe, spd):
@@ -160,23 +161,16 @@ class enemyProjectile:
      self.projDmg = projDmg
      self.projShots = projShots
      
-def gameScreen(p, pProj, playerRect, window): 
-        colour = {"white" : (255, 255, 255)
-                  , "black" : (0, 0, 0)
-                  , "red" : (255, 0, 0)
-                  , "green" : (0, 255, 0)
-                  , "blue" : (0, 0, 255)}
-
+def gameScreen(p, pProj, playerRect, window, colour): 
         window.fill(colour["white"])
-        pProj.draw(colour["red"], window)
+        pProj.draw(colour, window)
         p.draw(playerRect, window)
         pygame.display.update()  
 
-def main(p, pProj):
+def main(p, pProj, colour):
     width, height = 1000, 500
     window = pygame.display.set_mode((width, height))
     pygame.display.set_caption("Mystic Maze")
-
     playerRect = pygame.Rect(500, 250, p.width, p.height)
     clock = pygame.time.Clock()
     run = True
@@ -209,16 +203,17 @@ def main(p, pProj):
         keysPressed = pygame.key.get_pressed()
         pProj.update()   
         p.update(playerRect, keysPressed)
-        gameScreen(p, pProj, playerRect, window)
+        gameScreen(p, pProj, playerRect, window, colour)
     pygame.quit()
 
 playerStats = {"hp" : 100, "atk" : 1, "def" : 1, "spd" : 1}
-basicWandStats = {""}
+basicWand = {"projLife" : 10 , "baseDmg" : 1, "noShots" : 1, "spd" : 7.5, "size" : 5}
+colour = {"white" : (255, 255, 255), "black" : (0, 0, 0), "red" : (255, 0, 0), "green" : (0, 255, 0), "blue" : (0, 0, 255), "yellow" : (255, 255, 0), "orange" : (255, 102, 0), "purple" : (102, 0, 102) }
 
 fps = 60 
 
 p = player(playerStats["hp"], playerStats["atk"], playerStats["def"], playerStats["spd"])
-pProj = playerProjectile(1, 1, 1, 1)
+pProj = playerProjectile(basicWand["projLife"], basicWand["baseDmg"], basicWand["noShots"], basicWand["spd"], basicWand["size"])
     
 if __name__ == "__main__":
-    main(p, pProj)
+    main(p, pProj, colour)
